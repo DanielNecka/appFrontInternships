99da44c0e4c8dee82d9634a09a9c755c38f0d7aa
DanielNecka <neckadaniel@gmail.com>
2025-11-24 09:56:43 +0100

    feat: add CarsModule and MainCars component with routing feat: implement HttpClientModule in app module feat: create CarsService for car-related API calls feat: add main-cars component template

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { CarsModule } from './cars/cars-module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,8 @@ import { App } from './app';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners()
---
+++ src/app/cars/cars-module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainCars } from './main-cars/main-cars';
+
+@NgModule({
+  declarations: [
+    MainCars
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CarsModule { }
