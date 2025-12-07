7842fca71d13fd66170bbd41aac6be3fffad37c5
DanielNecka <neckadaniel@gmail.com>
2025-12-07 22:11:36 +0100

    feat: add footer component and update layout styles for improved UI

--- src/app/shared/shared-module.ts
+++ src/app/shared/shared-module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Header } from './header/header';
+import { Footer } from './footer/footer';
 
 
 
 @NgModule({
   declarations: [
-    Header
+    Header,
+    Footer
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    Header
+    Header,
+    Footer
   ]
 })
 export class SharedModule { }
